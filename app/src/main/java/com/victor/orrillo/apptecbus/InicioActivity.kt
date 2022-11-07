4709f1ebfcd844c64e93284708476b564f2e859a
dannycastillotecsup <danny.castillo@tecsup.edu.pe>
2022-11-07 15:27:39 -0500

    Ventana de inicio

---
+++ app/src/main/java/com/victor/orrillo/apptecbus/InicioActivity.kt
@@ -0,0 +1,11 @@
+package com.victor.orrillo.apptecbus
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class InicioActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_inicio)
+    }
+}
