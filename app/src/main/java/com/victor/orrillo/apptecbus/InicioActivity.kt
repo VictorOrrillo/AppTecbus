b53287653506a929a84300ceb7d1eb6c2ca6cdcb
VictorOrrillo <victor.orillo@tecsup.edu.pe>
2022-11-08 11:39:38 -0500

    actualizado

--- app/src/main/java/com/victor/orrillo/apptecbus/InicioActivity.kt
+++ app/src/main/java/com/victor/orrillo/apptecbus/InicioActivity.kt
@@ -1,11 +1,30 @@
 package com.victor.orrillo.apptecbus
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import com.google.firebase.auth.FirebaseAuth
 
 class InicioActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_inicio)
+
+        auth = FirebaseAuth.getInstance()
+
+        val email = intent.getStringExtra("email")
+        val displayName = intent.getStringExtra("name")
+
+        findViewById<TextView>(R.id.textView).text = displayName + "\n" + email
+
+        findViewById<Button>(R.id.gcerrar).setOnClickListener {
+            auth.signOut()
+            startActivity(Intent(this, MainActivity::class.java))
+        }
     }
 }
