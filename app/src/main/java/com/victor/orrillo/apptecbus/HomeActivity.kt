ba9a3cddc0e48142f0e828290da6c48a07c8e21f
VictorOrrillo <victor.orillo@tecsup.edu.pe>
2022-11-08 02:04:42 -0500

    MENU ARREGLADO FALTAN MEJORAR BOTONES

--- app/src/main/java/com/victor/orrillo/apptecbus/HomeActivity.kt
+++ app/src/main/java/com/victor/orrillo/apptecbus/HomeActivity.kt
@@ -9,19 +9,18 @@ import com.google.firebase.auth.FirebaseAuth
 
 class HomeActivity : AppCompatActivity() {
 
-
     private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        setContentView(R.layout.activity_inicio)
 
         auth = FirebaseAuth.getInstance()
 
         val email = intent.getStringExtra("email")
         val displayName = intent.getStringExtra("name")
 
-        findViewById<TextView>(R.id.textView).text = email + "\n" + displayName
+        findViewById<TextView>(R.id.textView).text = displayName + "\n" + email
 
         findViewById<Button>(R.id.gcerrar).setOnClickListener {
             auth.signOut()
