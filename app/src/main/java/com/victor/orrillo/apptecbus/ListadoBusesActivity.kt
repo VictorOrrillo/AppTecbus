dea96be82d4351eb53116b53c3d1156198006cca
dannycastillotecsup <danny.castillo@tecsup.edu.pe>
2022-11-07 20:04:50 -0500

    Listado de buses

---
+++ app/src/main/java/com/victor/orrillo/apptecbus/ListadoBusesActivity.kt
@@ -0,0 +1,11 @@
+package com.victor.orrillo.apptecbus
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ListadoBusesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_listado_buses)
+    }
+}
